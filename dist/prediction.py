5ec330fd8a5ae824c0d5cbcd7e65ec4e9370a6c7
ChenemiKen <chenemiken15@gmail.com>
2023-11-24 23:20:01 +0100

    herty yelp

--- dist/prediction.py
+++ dist/prediction.py
@@ -1,22 +1,21 @@
 import pickle
 import sys
 
-# with open('RandomForest.pkl', 'rb') as model_file:
-#     model = pickle.load(model_file)
+with open('RandomForest.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
 
 
-# def predict(input_data):
-#     prediction = model.predict([input_data])
-#     return prediction.tolist()
+def predict(input_data):
+    prediction = model.predict([input_data])
+    return prediction.tolist()
 
 
-# # input_data = sys.stdin.read()
-# temperature = float(sys.argv[1])
-# humidity = float(sys.argv[2])
-# ph = float(sys.argv[3])
-# water = float(sys.argv[4])
-# crop = float(sys.argv[5])
+# input_data = sys.stdin.read()
+temperature = float(sys.argv[1])
+humidity = float(sys.argv[2])
+ph = float(sys.argv[3])
+water = float(sys.argv[4])
+crop = float(sys.argv[5])
 
-# result = predict([temperature, humidity, ph, water, crop])
-# print(result[0])
-print("string")
+result = predict([temperature, humidity, ph, water, crop])
+print(result[0])
