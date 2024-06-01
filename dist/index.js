afcdcf2ef95566e262348a3b91c0559cb290ffaa
ChenemiKen <chenemiken15@gmail.com>
2024-06-01 23:14:13 +0100

    added readme doc

--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 require("dotenv/config");
-const service_1 = require("./service");
 const port = process.env.PORT || 8000;
 app_1.default.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
-(0, service_1.makePrediction)(23.45, 50.61, 6.07, 173.56, 1.0).then(m => console.log(m));
+// makePrediction(23.45, 50.61, 6.07, 173.56, 1.0).then(m => console.log(m))
+// console.log(getPlanting(seasons.winter, "maize"))
