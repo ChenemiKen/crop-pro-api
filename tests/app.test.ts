2b4662b4bd145efb125d649750b90a5a9eb27799
ChenemiKen <chenemiken15@gmail.com>
2024-06-07 00:31:44 +0100

    testing prediction endpoint

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,6 +1,6 @@
 import { describe} from "node:test";
-// import app from "../src/app";
-
+const request = require("supertest");
+import app from "../src/app";
 
 describe("Base test", () => {
     test('adds 1 + 2 to equal 3', () => {
@@ -8,8 +8,27 @@ describe("Base test", () => {
     });
 })
 
-// describe("GET: /", () => {
-//     it("should return Crop Pro", () => {
-//         expect("").toBe("");
-//     })
-// })
+describe("GET: /", () => {
+    it("should return Crop pro", async () => {
+        const response = await request(app).get("/")
+        expect(response.status).toEqual(200)
+        expect(response.text).toEqual("Crop pro")
+    })
+})
+
+describe("POST: /predict", () => {
+    it("should return prediction info", async () => {
+        const response = await request(app).post("/predict")
+            .send({
+                "temperature":23,
+                "humidity":4.54,
+                "country": "NG",
+                "ph": 7,
+                "waterAvailability": 78,
+                "selectedCrop": "rice"
+            })
+        expect(response.status).toEqual(200)
+        console.log(response.body)
+        expect(response.body).toEqual("Crop pro")
+    })
+})
